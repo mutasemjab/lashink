c40f0ac288f7eff5176081a9e7b9f2b487ffb4be
Khaled Abdallah <khaledabdalah488@gmail.com>
2026-09-14 13:30:17 +0300

    add route for admin redirection and create .gitignore for session storage

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
+Route::get('/', fn () => redirect('/admin'));
+
 Route::group([
     'prefix'     => LaravelLocalization::setLocale(),
     'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'website.mode'],
